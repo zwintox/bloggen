4c89c8291e7e0f91162e79e96d3cc09c7e21a06b
Magnus Jonsson <jonsson.magnus@outlook.com>
2018-08-15 10:57:29 +0200

    adderat set&get samt constr till users

--- src/main/java/com/example/blogg/users.java
+++ src/main/java/com/example/blogg/users.java
@@ -1,4 +1,43 @@
 package com.example.blogg;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+
+    public users() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
