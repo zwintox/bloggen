1f4babc86cd2216ae01ffb39b77d7cc444f3f5ac
Magnus Jonsson <jonsson.magnus@outlook.com>
2018-08-15 10:12:58 +0200

    uppdaterat POM

---
+++ bloggen/src/main/java/com/example/blogg/BloggApplication.java
@@ -0,0 +1,12 @@
+package com.example.blogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggApplication.class, args);
+    }
+}
---
+++ bloggen/src/test/java/com/example/blogg/BloggApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.blogg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BloggApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
