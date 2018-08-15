909d8f3972af53e03273d8ebca12dbc4b85a0042
matilda grannas <e.h.matilda@gmail.com>
2018-08-15 11:07:00 +0200

    added posts

--- src/main/java/com/example/blogg/posts.java
+++ src/main/java/com/example/blogg/posts.java
@@ -1,4 +1,60 @@
 package com.example.blogg;
 
-public class posts {
-}
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+    @Entity
+    public class posts {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String headline;
+        private String storytext;
+        private String post;
+
+        public posts() {
+
+        }
+
+        public posts (String name) {
+            this.post = post;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getPost() {
+            return post;
+        }
+
+        public void setPost(String name) {
+            this.post = post;
+        }
+
+        public String getHeadline() {
+            return headline;
+        }
+
+        public void setHeadline(String headline) {
+            this.headline = headline;
+        }
+
+
+        public String getStorytext() {
+            return storytext;
+        }
+
+        public void setStorytext(String storytext) {
+            this.storytext = storytext;
+        }
+    }
+
